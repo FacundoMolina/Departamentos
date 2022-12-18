de75e2f7ab36c777c113530eec4f363d3151f673
Facundo Molina <facundomolina2013@gmail.com>
2022-12-17 21:32:00 -0300

    Optimizado

--- src/app/servicios/departamentos.service.ts
+++ src/app/servicios/departamentos.service.ts
@@ -34,13 +34,13 @@ export class DepartamentosService {
           img:["assets/img/Santa Fe 5/SantaFe50.jpeg","assets/img/Santa Fe 5/SantaFe51.jpeg","assets/img/Santa Fe 5/SantaFe52.jpeg","assets/img/Santa Fe 5/SantaFe53.jpeg"],
           isFavorite : false,
         },
-        {
+/*         {
           id:3,
           nombre: "Departamento 4",
           descripcion: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
           img:["assets/img/Depto10.jpg","assets/img/Depto11.jpg","assets/img/Depto12.jpg"],
           isFavorite : true,
-        }
+        } */
       ];
 
     constructor() {
